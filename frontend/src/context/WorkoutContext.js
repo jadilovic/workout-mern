97a25a8b1ea6447c05f6350bab563165f9491c34
jadilovic <adilovic79@yahoo.com>
2023-01-30 20:34:07 +0100

    context

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+export const WorkoutContext = createContext();
+
+export const WorkoutContextProvider = ({ children }) => {
+	return <WorkoutContext.Provider>{children}</WorkoutContext.Provider>;
+};
