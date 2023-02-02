d54e835f2d0e6d56ab43d7f850a096cf67b16086
jadilovic <adilovic79@yahoo.com>
2023-02-02 10:39:16 +0100

    fix to static build

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,9 @@ const port = process.env.PORT || 4000;
 
 if (process.env.NODE_ENV === 'production') {
 	//*Set static folder up in production
-	app.use(express.static('frontend/build'));
+	//app.use(express.static('frontend/build'));
 
-	app.get('*', (req, res) =>
+	app.get('/*', (req, res) =>
 		res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
 	);
 }
